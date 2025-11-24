4d297cf1285ac18f337d8b88247d1a950540fcff
Nitin Singh <nsr2k06@gmail.com>
2025-11-24 13:16:18 +0530

    Add files via upload

---
+++ To-Do-App/script.js
@@ -0,0 +1,40 @@
+const inputbox =document.getElementById("input-box");
+const listContainer =document.getElementById("list-container");
+
+function addtask(){
+    if(inputbox.value === ''){
+        alert("You Must Write Something !")
+    }
+    else{
+        let li =document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+
+    }
+    inputbox.value=""
+    savedata()
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        savedata()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata()
+    }
+},false);
+
+function savedata(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showtasks(){
+    listContainer.innerHTML=localStorage.getItem("data");
+
+}
+showtasks()
